6994f3e5ee59914a83178628a632667ebe3fd808
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-04-21 15:49:44 +0200

    k8s-web-to-nginx.yaml and nginx.yaml deployments were added

--- k8s-web-to-nginx/index.mjs
+++ k8s-web-to-nginx/index.mjs
@@ -1,22 +1,22 @@
-import express from 'express'
-import os from 'os'
+import express from 'express';
+import os from 'os';
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 
 app.get("/", (req, res) => {
-    const helloMessage = `<h1>Hello from the ${os.hostname()}</h1>`
-    console.log(helloMessage)
-    res.send(helloMessage)
-})
+    const helloMessage = `<h1>Hello from the ${os.hostname()}</h1>`;
+    console.log(helloMessage);
+    res.send(helloMessage);
+});
 
 app.get("/nginx", async (req, res) => {
-    const url = 'http://nginx'
+    const url = 'http://nginx';
     const response = await fetch(url);
     const body = await response.text();
-    res.send(body)
-})
+    res.send(body);
+});
 
 app.listen(PORT, () => {
-    console.log(`Web server is listening at port ${PORT}`)
-})
+    console.log(`Web server is listening at port ${PORT}`);
+});
